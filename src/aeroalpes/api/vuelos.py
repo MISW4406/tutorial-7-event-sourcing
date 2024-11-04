a4b8db339978361cb60c6e739e783d0a865bfcc1
James Montealegre <js.montealegre@uniandes.edu.co>
2024-11-03 23:26:02 -0500

    fix vuelos.py

--- src/aeroalpes/api/vuelos.py
+++ src/aeroalpes/api/vuelos.py
@@ -8,16 +8,15 @@ from flask import Response
 from aeroalpes.modulos.vuelos.aplicacion.mapeadores import MapeadorReservaDTOJson
 from aeroalpes.modulos.vuelos.aplicacion.comandos.crear_reserva import CrearReserva
 from aeroalpes.modulos.vuelos.aplicacion.queries.obtener_reserva import ObtenerReserva
-from aeroalpes.seedwork.aplicacion.comandos import ejecutar_commando
 from aeroalpes.seedwork.aplicacion.queries import ejecutar_query
+from aeroalpes.seedwork.infraestructura.despachadores import despachador_eventos  # Importa el despachador de eventos
 
 bp = api.crear_blueprint('vuelos', '/vuelos')
 
 @bp.route('/reserva', methods=('POST',))
 def reservar_usando_comando():
     try:
-        # NOTE Asignamos el valor 'pulsar' para usar la Unidad de trabajo de Pulsar y 
-        # no la defecto de SQLAlchemy
+        # Asignamos el valor 'pulsar' para usar la Unidad de trabajo de Pulsar
         session['uow_metodo'] = 'pulsar'
 
         reserva_dict = request.json
@@ -25,11 +24,15 @@ def reservar_usando_comando():
         map_reserva = MapeadorReservaDTOJson()
         reserva_dto = map_reserva.externo_a_dto(reserva_dict)
 
-        comando = CrearReserva(reserva_dto.fecha_creacion, reserva_dto.fecha_actualizacion, reserva_dto.id, reserva_dto.itinerarios)
+        comando = CrearReserva(
+            reserva_dto.fecha_creacion,
+            reserva_dto.fecha_actualizacion,
+            reserva_dto.id,
+            reserva_dto.itinerarios
+        )
         
-        # TODO Reemplaze es todo código sincrono y use el broker de eventos para propagar este comando de forma asíncrona
-        # Revise la clase Despachador de la capa de infraestructura
-        ejecutar_commando(comando)
+        # Enviar el comando al broker de eventos de forma asíncrona usando el despachador
+        despachador_eventos.publicar_comando(comando)
         
         return Response('{}', status=202, mimetype='application/json')
     except ExcepcionDominio as e:
