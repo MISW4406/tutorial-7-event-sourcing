5d4d28570d9cfca270e3286757172c13251b348f
Andrés Boyacá <jandresboyaca@gmail.com>
2025-07-20 16:12:38 +0000

    Fix devcon and github acitons

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -7,12 +7,3 @@ mkdir -p data/zookeeper
 mkdir -p data/mysql
 sudo chmod -R 777 ./data
 sudo chmod -R 777 ./connectors
-
-# Build docker images for local development
-if command -v docker >/dev/null 2>&1; then
-  docker build . -f aeroalpes.Dockerfile -t aeroalpes/flask
-  docker build . -f adaptador.Dockerfile -t aeroalpes/adaptador
-  docker build . -f notificacion.Dockerfile -t aeroalpes/notificacion
-  docker build . -f ui.Dockerfile -t aeroalpes/ui
-  docker-compose pull || true
-fi
